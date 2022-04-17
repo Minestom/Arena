d04792423f03b0378ad3710facce3d94e6ee6fc9
iam4722202468 <aceparent@gmail.com>
2022-04-17 18:00:50 -0400

    Team disbanding

--- src/main/java/net/minestom/arena/team/TeamManager.java
+++ src/main/java/net/minestom/arena/team/TeamManager.java
@@ -3,9 +3,10 @@ package net.minestom.arena.team;
 import net.minestom.server.entity.Player;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TeamManager {
-    private static final HashMap<Player, Team> teams = new HashMap<>();
+    private static final Map<Player, Team> teams = new HashMap<>();
 
     public static Team getTeam(Player player) {
         return teams.get(player);
@@ -13,5 +14,14 @@ public class TeamManager {
 
     public static void createTeam(Player player) {
         teams.put(player, new Team(player));
+    }
+    
+    public static void removeTeam(Player player) {
+        Team team = teams.get(player);
+
+        if (team != null) {
+            team.disband();
+            teams.remove(player);
+        }
     }
 }
