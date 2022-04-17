d3a253e13f146a2241cdc76b1a846419cf9063a9
Tristan <LeoDog896@hotmail.com>
2022-04-17 19:20:35 -0400

    Remove arena tag
    Switch to a memory based implementation if needed

--- src/main/java/net/minestom/arena/game/Arena.java
+++ src/main/java/net/minestom/arena/game/Arena.java
@@ -2,18 +2,13 @@ package net.minestom.arena.game;
 
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
-import net.minestom.server.event.instance.AddEntityToInstanceEvent;
 import net.minestom.server.event.instance.RemoveEntityFromInstanceEvent;
 import net.minestom.server.instance.Instance;
-import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public interface Arena {
-
-    @NotNull Tag<UUID> arenaTag = Tag.UUID("arena");
 
     @NotNull Instance getArenaInstance();
 
@@ -23,17 +18,9 @@ public interface Arena {
         // Register this arena
         MinecraftServer.getInstanceManager().registerInstance(getArenaInstance());
 
-        getArenaInstance().eventNode().addListener(AddEntityToInstanceEvent.class, (event) -> {
-            if (event.getEntity() instanceof Player player)
-                player.setTag(arenaTag, getArenaInstance().getUniqueId());
-        });
-
         getArenaInstance().eventNode().addListener(RemoveEntityFromInstanceEvent.class, (event) -> {
             // We don't care about entities, only players.
             if ((event.getEntity() instanceof Player)) return;
-
-            // If a player leaves the instance, remove the tag from them.
-            event.getEntity().removeTag(arenaTag);
 
             for (Player player : getArenaInstance().getPlayers()) {
                 // There is still a player in this instance which is not scheduled to be removed.
