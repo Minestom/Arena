c2937c7de0b8341dfb729067cb9341158b296be7
Tristan <LeoDog896@hotmail.com>
2022-04-17 19:24:32 -0400

    Abstract arena class (#2)

--- src/main/java/net/minestom/arena/game/Arena.java
+++ src/main/java/net/minestom/arena/game/Arena.java
@@ -1,68 +1,28 @@
 package net.minestom.arena.game;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.title.Title;
-import net.minestom.arena.combat.CombatEvent;
-import net.minestom.arena.mob.RandomMob;
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.adventure.audience.Audiences;
-import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.EntityCreature;
 import net.minestom.server.entity.Player;
-import net.minestom.server.event.EventFilter;
-import net.minestom.server.event.EventNode;
-import net.minestom.server.event.entity.EntityDeathEvent;
 import net.minestom.server.event.instance.RemoveEntityFromInstanceEvent;
-import net.minestom.server.event.trait.InstanceEvent;
-import net.minestom.server.tag.Tag;
+import net.minestom.server.instance.Instance;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CompletableFuture;
 
-public final class Arena {
+public interface Arena {
 
-    private static final ConcurrentHashMap<UUID, Arena> arenaList = new ConcurrentHashMap<>();
-    private static final Tag<UUID> arenaTag = Tag.UUID("arena");
+    @NotNull Instance getArenaInstance();
 
-    private final @NotNull ArenaInstance arenaInstance;
-    private int stage = 0;
+    CompletableFuture<Void> join(@NotNull Player player);
 
-    public void nextStage() {
-        stage++;
+    default void init() {
+        // Register this arena
+        MinecraftServer.getInstanceManager().registerInstance(getArenaInstance());
 
-        for (int i = 0; i < stage; i++) {
-            EntityCreature creature = RandomMob.random(stage);
-
-            creature.setInstance(arenaInstance, new Pos(0, 42, 0));
-        }
-
-        for (Player player : arenaInstance.getPlayers()) {
-            player.showTitle(Title.title(Component.text("Stage " + stage), Component.empty()));
-            player.sendMessage(Component.text("Stage " + stage));
-        }
-
-    }
-
-    public Arena() {
-        this.arenaInstance = new ArenaInstance();
-
-        // Register this
-        arenaList.put(arenaInstance.getUniqueId(), this);
-        MinecraftServer.getInstanceManager().registerInstance(this.arenaInstance);
-
-        CombatEvent.hook(arenaInstance.eventNode(), false);
-
-        arenaInstance.eventNode().addListener(RemoveEntityFromInstanceEvent.class, (event) -> {
+        getArenaInstance().eventNode().addListener(RemoveEntityFromInstanceEvent.class, (event) -> {
             // We don't care about entities, only players.
             if ((event.getEntity() instanceof Player)) return;
 
-            // If a player leaves the instance, remove the tag from them.
-            event.getEntity().removeTag(arenaTag);
-
-
-            for (Player player : arenaInstance.getPlayers()) {
+            for (Player player : getArenaInstance().getPlayers()) {
                 // There is still a player in this instance which is not scheduled to be removed.
                 if (player != event.getEntity()) {
                     return;
@@ -70,34 +30,10 @@ public final class Arena {
             }
 
             // All players have left. We can remove this instance.
-            MinecraftServer.getInstanceManager().unregisterInstance(arenaInstance);
-        });
-
-        arenaInstance.eventNode().addListener(EntityDeathEvent.class, (event) -> {
-            for (Entity entity : this.arenaInstance.getEntities()) {
-                if (!(entity instanceof Player)) {
-                    // TODO give money;
-                    return; // round hasn't ended yet
-                }
-            }
-
-            nextStage();
+            MinecraftServer.getInstanceManager().unregisterInstance(getArenaInstance());
         });
     }
 
-    public void start() {
-        nextStage();
-    }
-
-    public void join(@NotNull Player player) {
-        player.setInstance(arenaInstance, new Pos(0, 41, 0));
-        player.setTag(arenaTag, arenaInstance.getUniqueId());
-    }
-
-    public static Arena getArena(Player player) {
-        if (!player.hasTag(arenaTag)) return null;
-
-        return arenaList.get(player.getTag(arenaTag));
-    }
+    void start();
 
 }
--- src/main/java/net/minestom/arena/game/ArenaCommand.java
+++ src/main/java/net/minestom/arena/game/ArenaCommand.java
@@ -1,29 +1,45 @@
 package net.minestom.arena.game;
 
 import net.minestom.arena.Lobby;
+import net.minestom.arena.MessageUtils;
+import net.minestom.arena.game.mob.MobArena;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.condition.Conditions;
 import net.minestom.server.entity.Player;
 
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static net.minestom.server.command.builder.arguments.ArgumentType.Literal;
+
 public class ArenaCommand extends Command {
+
+    private final Map<String, Supplier<Arena>> arenas = Map.of(
+            "mob", MobArena::new
+    );
 
     public ArenaCommand() {
         super("arena");
         setCondition(Conditions::playerOnly);
 
         setDefaultExecutor((sender, context) -> {
-            Player player = (Player) sender;
+            MessageUtils.sendWarnMessage(sender, "Usage: /arena <name>. Choices are: " + String.join(", ", arenas.keySet()));
+        });
 
-            if (player.getInstance() != Lobby.INSTANCE) {
-                player.sendMessage("You are not in the lobby! Join the lobby first.");
-                return;
-            }
+        arenas.forEach((name, arena) -> {
+            addSyntax((sender, context) -> {
+                Player player = (Player) sender;
 
-            Arena arena = new Arena();
+                if (player.getInstance() != Lobby.INSTANCE) {
+                    player.sendMessage("You are not in the lobby! Join the lobby first.");
+                    return;
+                }
 
-            arena.join(player);
-            arena.start();
+                Arena suppliedArena = arena.get();
 
+                suppliedArena.join(player).thenRun(suppliedArena::start);
+
+            }, Literal(name));
         });
 
 
