77acb76d1039d6bea288d90207a1530980b57b77
TheMode <themode@outlook.fr>
2022-04-18 20:25:19 +0200

    Basic monitoring header/footer

--- src/main/java/net/minestom/arena/Main.java
+++ src/main/java/net/minestom/arena/Main.java
@@ -1,17 +1,26 @@
 package net.minestom.arena;
 
+import net.kyori.adventure.text.Component;
 import net.minestom.arena.game.ArenaCommand;
 import net.minestom.arena.group.GroupCommand;
 import net.minestom.arena.group.GroupEvent;
 import net.minestom.arena.utils.ServerProperties;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.adventure.audience.Audiences;
 import net.minestom.server.command.CommandManager;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.event.server.ServerTickMonitorEvent;
 import net.minestom.server.extras.lan.OpenToLAN;
 import net.minestom.server.extras.velocity.VelocityProxy;
+import net.minestom.server.monitoring.TickMonitor;
+import net.minestom.server.timer.TaskSchedule;
+import net.minestom.server.utils.MathUtils;
+
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicReference;
 
 public final class Main {
     public static void main(String[] args) {
@@ -38,6 +47,32 @@ public final class Main {
                 event.setSpawningInstance(Lobby.INSTANCE);
                 player.setRespawnPoint(new Pos(0, 42, 0));
             });
+
+            // Monitoring
+            AtomicReference<TickMonitor> lastTick = new AtomicReference<>();
+            handler.addListener(ServerTickMonitorEvent.class, event -> lastTick.set(event.getTickMonitor()));
+
+            // Header/footer
+            MinecraftServer.getSchedulerManager().scheduleTask(() -> {
+                Collection<Player> players = MinecraftServer.getConnectionManager().getOnlinePlayers();
+                if (players.isEmpty()) return;
+
+                final Runtime runtime = Runtime.getRuntime();
+                final TickMonitor tickMonitor = lastTick.get();
+                final long ramUsage = (runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024;
+
+                final Component header = Component.text("Minestom demo")
+                        .append(Component.newline()).append(Component.text("Players: " + players.size()))
+                        .append(Component.newline()).append(Component.newline())
+                        .append(Component.text("RAM USAGE: " + ramUsage + " MB").append(Component.newline())
+                                .append(Component.text("TICK TIME: " + MathUtils.round(tickMonitor.getTickTime(), 2) + "ms"))).append(Component.newline());
+                final Component footer = Component.newline().append(Component.text("Project: minestom.net").append(Component.newline())
+                        .append(Component.text("Source: github.com/Minestom/Minestom")).append(Component.newline())
+                        .append(Component.text("Arena: github.com/Minestom/Arena")));
+
+                Audiences.players().sendPlayerListHeaderAndFooter(header, footer);
+
+            }, TaskSchedule.tick(5), TaskSchedule.tick(5));
         }
 
         final String forwardingSecret = ServerProperties.getForwardingSecret();
