ae75c6b9467184ae9bada3b7976b8d9ccee7bce3
TheMode <themode@outlook.fr>
2022-04-18 20:09:58 +0200

    Request Audience

--- src/main/java/net/minestom/arena/Messenger.java
+++ src/main/java/net/minestom/arena/Messenger.java
@@ -1,21 +1,21 @@
 package net.minestom.arena;
 
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
-import net.minestom.server.command.CommandSender;
 
 public final class Messenger {
     public static final TextColor PINK_COLOR = TextColor.color(209, 72, 212);
     public static final TextColor ORANGE_COLOR = TextColor.color(232, 175, 53);
 
-    public static void info(CommandSender sender, String message) {
-        sender.sendMessage(Component.text("! ", PINK_COLOR)
+    public static void info(Audience audience, String message) {
+        audience.sendMessage(Component.text("! ", PINK_COLOR)
                 .append(Component.text(message, NamedTextColor.GRAY)));
     }
 
-    public static void warn(CommandSender sender, String message) {
-        sender.sendMessage(Component.text("* ", ORANGE_COLOR)
+    public static void warn(Audience audience, String message) {
+        audience.sendMessage(Component.text("* ", ORANGE_COLOR)
                 .append(Component.text(message, NamedTextColor.GRAY)));
     }
 }
