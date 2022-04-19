8df976ff8b26a30c5057eb2eb5caabde01d5e9da
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-04-19 15:50:46 +0100

    Restore health when using /leave (#14)

--- src/main/java/net/minestom/arena/LeaveCommand.java
+++ src/main/java/net/minestom/arena/LeaveCommand.java
@@ -1,7 +1,5 @@
 package net.minestom.arena;
 
-import net.minestom.arena.CommandUtils;
-import net.minestom.arena.Lobby;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.Player;
 
@@ -13,6 +11,7 @@ public final class LeaveCommand extends Command {
         setDefaultExecutor((sender, context) -> {
             final Player player = (Player) sender;
             player.setInstance(Lobby.INSTANCE);
+            player.setHealth(player.getMaxHealth());
         });
     }
 }
