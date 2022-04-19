1f5daaca115da102ae246402f3d54a8ca7faf39d
kiipy <25848425+kiipy@users.noreply.github.com>
2022-04-19 19:05:09 +0200

    Make mobs stronger every stage

--- src/main/java/net/minestom/arena/game/mob/ZombieMob.java
+++ src/main/java/net/minestom/arena/game/mob/ZombieMob.java
@@ -11,8 +11,8 @@ import java.time.Duration;
 import java.util.List;
 
 final class ZombieMob extends ArenaMob {
-    public ZombieMob(int level) {
-        super(EntityType.ZOMBIE);
+    public ZombieMob(int stage) {
+        super(EntityType.ZOMBIE, stage);
         addAIGroup(
                 List.of(new MeleeAttackGoal(this, 1.2, 20, TimeUnit.SERVER_TICK),
                         new FollowTargetGoal(this, Duration.of(3, TimeUnit.SERVER_TICK))),
