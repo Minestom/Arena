97b54ce66fe129ad4c1b04bda118994bd178c223
kiip1 <25848425+kiipy@users.noreply.github.com>
2022-04-20 20:53:32 +0200

    Create continue item and add stripItalics + hideFlags utils

--- src/main/java/net/minestom/arena/Items.java
+++ src/main/java/net/minestom/arena/Items.java
@@ -2,18 +2,17 @@ package net.minestom.arena;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
+import net.minestom.arena.utils.ItemUtils;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 
-import java.util.List;
-
 public final class Items {
-    public static final ItemStack COIN = ItemStack.builder(Material.SUNFLOWER)
-            .displayName(Component.text("Coin", NamedTextColor.GOLD).decoration(TextDecoration.ITALIC, false))
-            .lore(List.of(
-                    Component.empty(),
-                    Component.text("Use me to buy things!", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false)
-            ))
-            .build();
+    public static final ItemStack COIN = ItemUtils.stripItalics(ItemStack.builder(Material.SUNFLOWER)
+            .displayName(Component.text("Coin", NamedTextColor.GOLD))
+            .lore(Component.empty(), Component.text("Use me to buy things!", NamedTextColor.GRAY))
+            .build());
+    public static final ItemStack CONTINUE = ItemUtils.stripItalics(ItemStack.builder(Material.FEATHER)
+            .displayName(Component.text("Continue", NamedTextColor.RED))
+            .lore(Component.text("Click me to continue", NamedTextColor.GRAY))
+            .build());
 }
---
+++ src/main/java/net/minestom/arena/utils/ItemUtils.java
@@ -0,0 +1,41 @@
+package net.minestom.arena.utils;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.minestom.server.item.ItemHideFlag;
+import net.minestom.server.item.ItemStack;
+import org.jetbrains.annotations.Contract;
+
+import java.util.stream.Collectors;
+
+public final class ItemUtils {
+    private ItemUtils() {}
+
+    @Contract("null -> null; !null -> !null")
+    public static Component stripItalics(Component component) {
+        if (component == null) return null;
+
+        if (component.decoration(TextDecoration.ITALIC) == TextDecoration.State.NOT_SET) {
+            component = component.decoration(TextDecoration.ITALIC, false);
+        }
+
+        return component;
+    }
+
+    @Contract("null -> null; !null -> !null")
+    public static ItemStack stripItalics(ItemStack itemStack) {
+        if (itemStack == null) return null;
+
+        return itemStack.withDisplayName(ItemUtils::stripItalics)
+                .withLore(lore -> lore.stream()
+                        .map(ItemUtils::stripItalics)
+                        .collect(Collectors.toList()));
+    }
+
+    @Contract("null -> null; !null -> !null")
+    public static ItemStack hideFlags(ItemStack itemStack) {
+        if (itemStack == null) return null;
+
+        return itemStack.withMeta(builder -> builder.hideFlag(ItemHideFlag.values()));
+    }
+}
