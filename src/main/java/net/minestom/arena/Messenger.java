a2000f4b0d6a05c3e9e50f200019b42de6b4885e
kiip1 <25848425+kiipy@users.noreply.github.com>
2022-04-20 20:55:00 +0200

    Add countdown method in Messenger

--- src/main/java/net/minestom/arena/Messenger.java
+++ src/main/java/net/minestom/arena/Messenger.java
@@ -1,9 +1,17 @@
 package net.minestom.arena;
 
 import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.title.Title;
+import net.minestom.server.sound.SoundEvent;
+import net.minestom.server.timer.ExecutionType;
+import net.minestom.server.timer.Scheduler;
+import net.minestom.server.timer.TaskSchedule;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class Messenger {
     public static final TextColor PINK_COLOR = TextColor.color(209, 72, 212);
@@ -25,5 +33,22 @@ public final class Messenger {
     public static void warn(Audience audience, Component message) {
         audience.sendMessage(Component.text("* ", ORANGE_COLOR)
                 .append(message.color(NamedTextColor.GRAY)));
+    }
+
+    // TODO: use CompletableFuture instead
+    public static void countdown(Audience audience, int from, Runnable thenRun) {
+        final AtomicInteger countdown = new AtomicInteger(from);
+        Scheduler.newScheduler().submitTask(() -> {
+            final int count = countdown.getAndDecrement();
+            if (count <= 0) {
+                thenRun.run();
+                return TaskSchedule.stop();
+            }
+
+            audience.showTitle(Title.title(Component.text(count, NamedTextColor.GREEN), Component.empty()));
+            audience.playSound(Sound.sound(SoundEvent.BLOCK_NOTE_BLOCK_PLING, Sound.Source.BLOCK, 1, 1), Sound.Emitter.self());
+
+            return TaskSchedule.seconds(1);
+        }, ExecutionType.ASYNC);
     }
 }
