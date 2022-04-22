06404f39373710a22a7f91b0222767c8f4e87bf7
TheMode <themode@outlook.fr>
2022-04-23 00:29:30 +0200

    Create file system

--- src/main/java/net/minestom/arena/utils/ResourceUtils.java
+++ src/main/java/net/minestom/arena/utils/ResourceUtils.java
@@ -6,39 +6,42 @@ import java.net.URISyntaxException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public final class ResourceUtils {
     public static void extractResource(String source) throws URISyntaxException, IOException {
         URI uri = ResourceUtils.class.getResource("/" + source).toURI();
-        final Path jarPath = Paths.get(uri);
-        final Path target = Path.of(source);
-        if (Files.exists(target)) {
-            try (Stream<Path> pathStream = Files.walk(target)) {
-                pathStream.sorted(Comparator.reverseOrder())
-                        .forEach(path -> {
-                            try {
-                                Files.delete(path);
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                        });
+        try (FileSystem ignored = FileSystems.newFileSystem(uri, Map.of("create", "true"))) {
+            final Path jarPath = Paths.get(uri);
+            final Path target = Path.of(source);
+            if (Files.exists(target)) {
+                try (Stream<Path> pathStream = Files.walk(target)) {
+                    pathStream.sorted(Comparator.reverseOrder())
+                            .forEach(path -> {
+                                try {
+                                    Files.delete(path);
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                }
             }
-        }
-        Files.walkFileTree(jarPath, new SimpleFileVisitor<>() {
-            @Override
-            public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs) throws IOException {
-                Path currentTarget = target.resolve(jarPath.relativize(dir).toString());
-                Files.createDirectories(currentTarget);
-                return FileVisitResult.CONTINUE;
-            }
+            Files.walkFileTree(jarPath, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs) throws IOException {
+                    Path currentTarget = target.resolve(jarPath.relativize(dir).toString());
+                    Files.createDirectories(currentTarget);
+                    return FileVisitResult.CONTINUE;
+                }
 
-            @Override
-            public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
-                final Path to = target.resolve(jarPath.relativize(file).toString());
-                Files.copy(file, to, StandardCopyOption.REPLACE_EXISTING);
-                return FileVisitResult.CONTINUE;
-            }
-        });
+                @Override
+                public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+                    final Path to = target.resolve(jarPath.relativize(file).toString());
+                    Files.copy(file, to, StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
     }
 }
