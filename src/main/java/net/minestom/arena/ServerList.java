efb9e413595db0fdb9477b49e0a041be785f644c
kiip1 <25848425+kiipy@users.noreply.github.com>
2022-04-22 17:06:04 +0200

    Reduce favicon reading code

--- src/main/java/net/minestom/arena/ServerList.java
+++ src/main/java/net/minestom/arena/ServerList.java
@@ -9,34 +9,31 @@ import net.minestom.server.event.server.ServerListPingEvent;
 import net.minestom.server.ping.ResponseData;
 import net.minestom.server.ping.ServerListPingType;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.Base64;
 
 public final class ServerList {
-    public static void hook(EventNode<Event> eventNode) {
-        String favicon = "";
-        try {
-            final InputStream faviconStream = Main.class.getResourceAsStream("/favicon.png");
-            assert faviconStream != null;
-            BufferedImage image = ImageIO.read(faviconStream);
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", outputStream);
-            favicon = "data:image/png;base64," + Base64.getEncoder().encodeToString(outputStream.toByteArray());
-            outputStream.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    private static final String FAVICON;
 
-        String finalFavicon = favicon;
+    static {
+        String favicon = null;
+        try (InputStream stream = Main.class.getResourceAsStream("/favicon.png")) {
+            if (stream != null)
+                favicon = "data:image/png;base64," + Base64.getEncoder().encodeToString(stream.readAllBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FAVICON = favicon;
+    }
+
+    public static void hook(EventNode<Event> eventNode) {
         eventNode.addListener(ServerListPingEvent.class, event -> {
             final ResponseData responseData = event.getResponseData();
             final boolean rgb = event.getPingType() == ServerListPingType.MODERN_FULL_RGB;
 
             responseData.setDescription(Component.text("Minestom Arena").color(rgb ? Messenger.ORANGE_COLOR : NamedTextColor.GOLD));
-            responseData.setFavicon(finalFavicon);
+            if (FAVICON != null)
+                responseData.setFavicon(FAVICON);
             responseData.setMaxPlayer(100);
             responseData.addEntries(MinecraftServer.getConnectionManager().getOnlinePlayers());
         });
