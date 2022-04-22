2e7a496cc5f19d1078ac77803e61bccc96217a6b
kiip1 <25848425+kiipy@users.noreply.github.com>
2022-04-22 16:53:10 +0200

    Remove unchecked code

--- src/main/java/net/minestom/arena/feature/DropFeature.java
+++ src/main/java/net/minestom/arena/feature/DropFeature.java
@@ -1,16 +1,16 @@
 package net.minestom.arena.feature;
 
 import net.minestom.server.entity.ItemEntity;
-import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.item.ItemDropEvent;
+import net.minestom.server.event.trait.InstanceEvent;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.utils.time.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 
 record DropFeature() implements Feature {
     @Override
-    public void hook(@NotNull EventNode<Event> node) {
+    public void hook(@NotNull EventNode<InstanceEvent> node) {
         node.addListener(ItemDropEvent.class, event -> {
             ItemStack item = event.getItemStack();
             ItemEntity itemEntity = new ItemEntity(item);
