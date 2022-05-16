8b43035b3ccd1dfbbf08d10a138aba3803e4f5e3
kiip1 <25848425+kiip1@users.noreply.github.com>
2022-05-16 20:38:53 +0200

    Fix skeleton aim

--- src/main/java/net/minestom/arena/game/mob/SkeletonMob.java
+++ src/main/java/net/minestom/arena/game/mob/SkeletonMob.java
@@ -1,5 +1,7 @@
 package net.minestom.arena.game.mob;
 
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityProjectile;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.Player;
@@ -8,6 +10,7 @@ import net.minestom.server.entity.ai.target.ClosestEntityTarget;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.minestom.server.utils.time.TimeUnit;
+import org.jetbrains.annotations.Nullable;
 
 import java.time.Duration;
 import java.util.List;
@@ -21,8 +24,8 @@ final class SkeletonMob extends ArenaMob {
                 this, Duration.of(40, TimeUnit.SERVER_TICK),
                 16, 8, true, 1, 0.1);
 
-        rangedAttackGoal.setProjectileGenerator((entity) -> {
-            EntityProjectile projectile = new EntityProjectile(entity, EntityType.ARROW);
+        rangedAttackGoal.setProjectileGenerator(entity -> {
+            HomingArrow projectile = new HomingArrow(entity, EntityType.PLAYER);
             projectile.scheduleRemove(Duration.of(100, TimeUnit.SERVER_TICK));
             return projectile;
         });
@@ -31,5 +34,32 @@ final class SkeletonMob extends ArenaMob {
                 List.of(rangedAttackGoal),
                 List.of(new ClosestEntityTarget(this, 32, Player.class))
         );
+    }
+
+    private static final class HomingArrow extends EntityProjectile {
+        private final EntityType target;
+
+        public HomingArrow(@Nullable Entity shooter, EntityType target) {
+            super(shooter, EntityType.ARROW);
+            this.target = target;
+        }
+
+        @Override
+        public void tick(long time) {
+            super.tick(time);
+            if (instance == null) return;
+            if (isOnGround()) return;
+
+            for (Entity entity : instance.getNearbyEntities(position, 5.0)) {
+                if (entity.getEntityType() != target) continue;
+
+                final Vec target = position.withLookAt(entity.getPosition()).direction();
+                final Vec newVelocity = velocity.add(target);
+
+                setVelocity(newVelocity);
+
+                return;
+            }
+        }
     }
 }
